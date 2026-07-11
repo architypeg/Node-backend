071ab3ee5029e9a86c2ce4b561046c7cc9aee6bb
architypeg <200451331+architypeg@users.noreply.github.com>
2026-07-11 19:37:16 +0530

    Worked with node stream and buffers

--- L1.app.js
+++ L1.app.js
@@ -1,10 +1,46 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method);
+    const method = req.method;
+    const url = req.url;
+    if(url === '/'){
+         res.setHeader('Content-Type', 'text/html')
+         res.write('<html>');
+         res.write('<head><title>My first Node server</title></head>');
+         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+         res.write('</html>');
+         return res.end();
+    }
+    if(url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data', (chunk) =>{
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            
+            fs.writeFileSync('message.txt', message);
+
+        })
+        
+        res.statusCode = 302;
+        res.setHeader('Location', '/')
+        return res.end();
+    }
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>');
+    res.write('<head><title>My first Node server</title></head>');
+    res.write('<body><h1>Hellow from my first Node server</h1></body>');
+    res.write('</html>');
 });
 
 server.listen(3000);
+
 
 /* So notes along.. whole nodejs runs using a event driven architecture where
  if something happens this should execute. When there are listeners like in
