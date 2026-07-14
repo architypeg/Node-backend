5ead2fb02e704c08e042f8005fc8cfb0bb6440fc
architypeg <200451331+architypeg@users.noreply.github.com>
2026-07-14 10:12:56 +0530

    Added controllers for products and error

---
+++ controllers/products.js
@@ -0,0 +1,20 @@
+const Product = require('../models/product');
+
+exports.getAddProduct= (req, res, next) => {
+    res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'})
+};
+
+exports.postAddProduct = (req, res, next) => {
+    const product = new Product(req.body.title)
+    res.redirect('/');  // Aside Note: Both of the routes can have the same name if the type of the request is different one is post and another is get.
+};
+
+exports.getProducts = (req, res, next) => {
+    const products = Product.fetchAll();
+    res.render('shop', {
+        prods: products, 
+        pageTitle: 'Shop', 
+        path: '/',
+        
+    });
+}
--- routes/admin.js
+++ routes/admin.js
@@ -1,22 +1,13 @@
 const express = require('express');
-const path = require('path');
-const rootDir = require('../util/path');
 
+const productsController = require('../controllers/products')
 
 const router = express.Router();
 
-const products = [];
-
 // /admin/add-product => GET
+router.get('/add-product', productsController.getAddProduct);
 
-router.get('/add-product',(req, res, next) => {
-    res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'})
-});
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-    products.push({title: req.body.title});
-    res.redirect('/');  // Aside Note: Both of the routes can have the same name if the type of the request is different one is post and another is get.
-}); 
+router.post('/add-product', productsController.postAddProduct); 
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
