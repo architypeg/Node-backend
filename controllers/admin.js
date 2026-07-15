d4b7f947ec4cbbd51a925e638aa8dd47c8483d52
architypeg <200451331+architypeg@users.noreply.github.com>
2026-07-15 17:14:14 +0530

    Added more routes and pages and wrote the controllers for them

---
+++ controllers/admin.js
@@ -0,0 +1,25 @@
+const Product = require('../models/product');
+
+exports.getAddProduct= (req, res, next) => {
+    res.render('admin/add-product', {pageTitle: 'Add Product', path: '/admin/add-product'})
+};
+
+exports.postAddProduct = (req, res, next) => {
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description;
+    const product = new Product(title, imageUrl, description, price)
+    product.save();
+    res.redirect('/');  // Aside Note: Both of the routes can have the same name if the type of the request is different one is post and another is get.
+};
+
+exports.getProducts = (req, res, next) =>{
+    Product.fetchAll((products) =>{
+        res.render('admin/products', {
+        prods: products, 
+        pageTitle: 'Admin Products', 
+        path: '/admin/products',  
+    });
+    }); 
+}
